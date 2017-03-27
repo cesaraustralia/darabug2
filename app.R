1547cbb8af74334340ee6246131f5dad6ea2e988
James Maino <James Maino>
2017-03-27 14:56:15 +1100

    fixed widht and height of phenology plot to avoid annoying transparency issue.

--- app.R
+++ app.R
@@ -101,14 +101,14 @@ ui <-
                                             HTML('<br/><br/><br/>')
                                      )
                                    ),
-                                   fluidRow(
+                                   # fluidRow(
                                      div(
                                        style = "position:relative",
                                        plotOutput("phenology", 
                                                   hover = hoverOpts("plot_hover", delay = 100, delayType = "debounce")),
                                        uiOutput("hover_info")
                                      )
-                                   )
+                                   # )
                                    )
                           )    
                      ),
@@ -253,7 +253,8 @@ server <- function(input, output, session){
     }
   })
   newEntry <- observe({
-    if(input$update>0) {
+    if(input$update>=0){
+      
       location<-paste0(isolate(input$location),', Australia')
       if(nchar(location) <16)location = 'Diamond Creek, VIC 3089' # if unrealistic location length
       fullLocation <- geocode(location, output = 'more')
@@ -263,14 +264,13 @@ server <- function(input, output, session){
           startDay<-as.numeric(format(input$startDate,'%j'))
           TMAX <- extract(Tmax, matrix(c(fullLocation$lon, fullLocation$lat), ncol = 2))
           TMIN <- extract(Tmin, matrix(c(fullLocation$lon, fullLocation$lat), ncol = 2))
-          startStage<-as.numeric(input$startStage)
+          startStage<-ifelse(is.null(input$startStage),2,as.numeric(input$startStage))
           insect<-getBug(input$species)
           location<-paste0(fullLocation$locality,', ', fullLocation$administrative_area_level_1)
           # browser()
           data<-develop(TMAX,TMIN, startDay, startStage, insect)
           
         })
-        # browser()
         updateTextInput(session, "location", value = location)
       })
       df<-as.data.frame(data[1,,])
@@ -291,64 +291,11 @@ server <- function(input, output, session){
       })
     }
   })
-  output$phenology<- renderPlot({
-    if(input$update>0){
-      if(isolate(length(values$df))){
-        if(TRUE){
-          data<-values$df
-          data$life<-factor(data$life)
-          if('adult'%in%levels(data$life))
-            data$life<-factor(data$life, levels =  c(levels(factor(data$life))[-1],levels(factor(data$life))[1])  ,ordered = TRUE)
-         
-          
-          # setPoints = list()
-          # for(i in 1:nrow(values$setpoints)){
-          #   x<-isolate(as.Date(values$setpoints[i,1]))
-          #   y<-isolate(values$setpoints[i,2])
-          #   setPoints[[i]]<-geom_point(aes(x=x,y=y),
-          #                                shape = 124, size = 10,colour = 'red')
-          # }
-  
-          p<-ggplot(data)+
-            geom_line(aes(value, species, colour = life),size = 6) +
-            geom_point(aes(value, species), colour = 'black', size=6)+
-            ylab(NULL) +
-            xlab(NULL) +
-            # setPoints+
-            geom_vline(xintercept = isolate(as.numeric(input$startDate)))+
-            geom_text(aes(x=isolate(input$startDate), label="date observed", y=data$species[1]), colour=rgb(0.5,0.5,0.5), vjust = 2.2,hjust = .33)+
-            scale_x_date(limits = c(min(data$value), max(data$value)),
-                         date_breaks = "1 weeks",date_minor_breaks = '1 week',
-                         date_labels = "%d %b" )+
-            theme_bw()+
-            theme(text = element_text(size=20,family='Nirmala UI', color = 'white'),
-                  axis.text.x = element_text(color = 'white',angle=45, vjust=1, hjust=1),
-                  axis.text.y = element_text(color = 'white'),
-                  legend.title=element_blank(),
-                  legend.key = element_blank(),
-                  panel.grid.minor = element_blank(), 
-                  panel.grid.major = element_blank(),
-                  panel.border = element_blank(),
-                  panel.background = element_blank(),
-                  legend.background = element_blank(),
-                  axis.line.x = element_line(color="white"),
-                  plot.background = element_blank()
-            )
-          # # browser()
-          # p<-ggplot(data ,aes(value, Stage_duration, colour = life))+
-          #     geom_point(colour = 'black')
-          return(p)
-        }
-      }
-    }else{return(NULL)}
-
-  },bg="transparent")
   output$hover_info <- renderUI({
     # browser()
     input$plot_hover
     data<-isolate(values$df)
     if(length(data)>0){
-      
       hover <- input$plot_hover
       point <- nearPoints(data, coordinfo = hover, threshold = 10, maxpoints = 1, addDist = TRUE)
       if (nrow(point) == 0) return(NULL)
@@ -375,12 +322,66 @@ server <- function(input, output, session){
                       "<b> ",point$variable,": </b>", format(point$value,'%d-%b'), "<br/>",
                       "<b> Duration (d): </b>", round(point$Stage_duration,1), "<br/>"
                       # "<b> Distance from left: </b>", left_px, "<b>, from top: </b>", top_px
-                      )))
+        )))
       )
     }else{return(NULL)}
   })
+  output$phenology<- renderPlot({
+    mytheme<-theme_bw()+
+              theme(text = element_text(size=20,family='Nirmala UI', color = 'white'),
+                    axis.text.x = element_text(color = 'white',angle=45, vjust=1, hjust=1),
+                    axis.text.y = element_text(color = 'white'),
+                    legend.title=element_blank(),
+                    legend.key = element_blank(),
+                    panel.grid.minor = element_blank(),
+                    panel.grid.major = element_blank(),
+                    panel.border = element_blank(),
+                    panel.background = element_blank(),
+                    legend.background = element_blank(),
+                    axis.line.x = element_line(color="white"),
+                    plot.background = element_blank()
+                    )
+    if(input$update>=0){
+      if(isolate(length(values$df))){
+        if(TRUE){
+          data<-values$df
+          data$life<-factor(data$life)
+          if('adult'%in%levels(data$life))
+            data$life<-factor(data$life, levels =  c(levels(factor(data$life))[-1],levels(factor(data$life))[1])  ,ordered = TRUE)
+
+
+          # setPoints = list()
+          # for(i in 1:nrow(values$setpoints)){
+          #   x<-isolate(as.Date(values$setpoints[i,1]))
+          #   y<-isolate(values$setpoints[i,2])
+          #   setPoints[[i]]<-geom_point(aes(x=x,y=y),
+          #                                shape = 124, size = 10,colour = 'red')
+          # }
+          weekspan<-as.numeric( max(data$value)-min(data$value))/7
+          p<-ggplot(data)+
+            geom_line(aes(value, species, colour = life),size = 6) +
+            geom_point(aes(value, species), colour = 'black', size=6)+
+            ylab(NULL) +
+            xlab(NULL) +
+            # setPoints+
+            geom_vline(xintercept = isolate(as.numeric(input$startDate)))+
+            geom_text(aes(x=isolate(input$startDate), label="date observed", y=data$species[1]), colour=rgb(0.5,0.5,0.5), vjust = 2.2,hjust = .33)+
+            scale_x_date(limits = c(min(data$value), max(data$value)),
+                         date_breaks = paste(ifelse(weekspan>20,4,1),"weeks"),date_minor_breaks = '1 week',
+                         date_labels = "%d %b" ) +
+                         mytheme
+
+          # browser()
+          # p<-ggplot(data ,aes(value, Stage_duration, colour = life))+
+          #     geom_point(colour = 'black')
+          return(p)
+        }
+      }
+    }else{return(NULL)}
+
+  },bg="transparent", width = 1000, height = 500)
   output$downloadData.csv <- downloadHandler(
-    filename = function() { paste(input$dataset, '.csv', sep='') },
+    filename = function() { paste('data_darabug.csv', sep='') },
     content = function(file) {
       df<-values$df
       dfw<-dcast(df,formula = species + location + life + stage + Stage_duration ~variable)
@@ -395,16 +396,16 @@ server <- function(input, output, session){
     insect<-getBug(input$species2)
     stageList<-lapply(1:length(names(insect$dev.funs)), FUN = function(x) x)
     names(stageList)<-names(insect$dev.funs)
-    selectInput("startStage2", label = h5("2. Life stage observed:"), 
-                choices = stageList, 
+    selectInput("startStage2", label = h5("2. Life stage observed:"),
+                choices = stageList,
                 selected = 1, width = '100%')
   })
   output$endStage2 <- renderUI({
     insect<-getBug(input$species2)
     stageList<-lapply(1:length(names(insect$dev.funs)), FUN = function(x) x)
     names(stageList)<-names(insect$dev.funs)
-    selectInput("endStage2", label = h5("6. Life stage to predict:"), 
-                choices = stageList, 
+    selectInput("endStage2", label = h5("6. Life stage to predict:"),
+                choices = stageList,
                 selected = length(stageList), width = '100%')
   })
     # A reactive expression that returns a cropped raster based on screen bounds
@@ -422,9 +423,9 @@ server <- function(input, output, session){
     })
     pal <- reactive( {
       # browser()
-              # if 
+              # if
               # tryCatch(colorNumeric(c("#EE0000","#B2DFEE","#007f00"), values(rasterBounds()),
-              #           na.color = "transparent"), 
+              #           na.color = "transparent"),
               #          error = colorNumeric(c("#EE0000","#B2DFEE","#007f00"), 1,
               #                               na.color = "transparent"))
       colorNumeric(c("#EE0000","#B2DFEE","#007f00"), values(rasterBounds()),
@@ -443,10 +444,10 @@ server <- function(input, output, session){
     observe({
       if(input$update2>0&!all(is.na(values(rasterBounds())))){
         # browser()
-        leafletProxy('map2')%>% 
-          removeTiles(layerId="rasimg") %>% 
+        leafletProxy('map2')%>%
+          removeTiles(layerId="rasimg") %>%
           addRasterImage(rasterBounds(), opacity=0.5, layerId = 'rasimg',colors = pal()) %>%
-          clearControls() %>% 
+          clearControls() %>%
         addLegend(pal = pal(), values = values(rasterBounds()),
                   title = paste0(names(getBug(input$species2)$dev.funs)[as.integer(input$endStage2)],
                                  ' monitoring \n date'), position = 'bottomleft',
@@ -458,8 +459,8 @@ server <- function(input, output, session){
           clearControls()
         }
     })
-    TMAX <- Tmax+0.000001 # save to memory with +0.000001
-    TMIN <- Tmin+0.000001
+    # TMAX <- Tmax+0.000001 # save to memory with +0.000001
+    # TMIN <- Tmin+0.000001
     observe({
       if(input$update2 > 0) {
         withProgress(message = "LOADING. PLEASE WAIT...", { # create progress bar
@@ -496,7 +497,7 @@ server <- function(input, output, session){
         })
       }
     })
-    
+
     ########################## INSECT PLOT PAGE ###########################
     # include about here
     output$tempresponse<- renderPlot(bg="transparent",{
@@ -505,13 +506,13 @@ server <- function(input, output, session){
       df <- data.frame(temp = temps)
       stages<-names(insect$dev.funs)
       for (stage in stages){
-        df[,stage]<-insect$dev.funs[[stage]](temps)  
+        df[,stage]<-insect$dev.funs[[stage]](temps)
       }
-      dl<-reshape2::melt(df, id = 'temp', variable.name = 'stage', 
+      dl<-reshape2::melt(df, id = 'temp', variable.name = 'stage',
                          value.name = 'dev')
       p<-ggplot() + geom_line(data = dl, aes(x = temp, y = dev, linetype = stage, color = stage)) +
         xlab('Temperature (C)')+
-        ylab('Development rate (1/d)')+ 
+        ylab('Development rate (1/d)')+
         theme(text = element_text(size=20, colour = 'white'), #family='Nirmala UI',
               axis.text = element_text(size=20, colour = 'white'),
               legend.title=element_blank(),
@@ -526,15 +527,15 @@ server <- function(input, output, session){
               plot.background = element_blank(),
               axis.ticks = element_line(color = "white")
         )
-      
-      
+
+
       return(p)
     })
     output$source<-reactive({
       insect<-getBug(input$species3)
       return(paste('Source:', insect$source))
-      })  
-    
+      })
+
   } # server(...
 
 
